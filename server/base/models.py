fccc942ed7b772458feeabdcee35b4a6cc4f407e
Dhruvin711 <dhruvin7117@gmail.com>
2023-09-09 18:40:28 +0530

    Added Experience-Model, views, and Urls

--- server/base/models.py
+++ server/base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Experince(models.Model):
+    company_name = models.CharField(max_length=100)
+    company_year = models.IntegerField()
+
+    post_username = models.CharField(max_length=100)
+    post_time = models.CharField(max_length=100)
+
+    # rounds = ArrayField(
+    #     ArrayField(
+    #         models.TextField(), size = 2,
+    #     )
+    # )
+    tips = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.company_name
--- server/base/views.py
+++ server/base/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return True
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email = email)
+        except:
+            return False
+        
+        user = authenticate(request, email = email, password = password)
+
+        if user is not None:
+            login(request, user)
+            return True
+        
+        else:
+            return False
+    
+    return False
+
+def registerPage(request):
+    return null
+
+def logoutUser(request):
+    logout(request)
+    return True
+
+def homePage(request):
+    return HttpResponse("Home page")
