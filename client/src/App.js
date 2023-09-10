dbcfc6b3c5e338d4fc9fbbe4d7a70f58660bcc3c
Parthkumar Patel <75678394+parthpatel021@users.noreply.github.com>
2023-09-10 13:18:45 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Auth />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/register" element={<Register />} /> 
         </Routes>
