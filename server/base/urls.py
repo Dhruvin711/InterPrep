a4df03d51652f51db6ddd068ba31141f4ce3688e
Sarthak <sarthakvkumar@gmail.com>
2023-09-11 10:41:23 +0530

    added /createExperience Route

--- server/base/urls.py
+++ server/base/urls.py
@@ -1,22 +1,11 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from . import views
-# from django.conf import settings
-
-# urlpatterns = [
-#     path('login/', views.UserLogin.as_view(), name='login'),
-#     path('register/', views.UserRegister.as_view(), name='register'),
-#     path('logout/', views.UserLogout.as_view(), name='logout'),
-#     path('', views.homePage, name='home'),
-# ]
-
 from . import views
 from django.urls import path
-from .views import register_user, user_login, user_logout
+from .views import register_user, user_login, user_logout,createExperience
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
+    path('createExperience/', createExperience, name='create-experience'),
     path('', views.homePage, name='home'),
 ]
