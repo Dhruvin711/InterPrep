72144f386ade86672dd823394529d17cbb4a8471
Dhruvin711 <dhruvin7117@gmail.com>
2023-09-11 18:13:09 +0530

    Updated Login and Register views

--- server/base/admin.py
+++ server/base/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Experience
-from .models import CustomUser
 
 admin.site.register(Experience)
-admin.site.register(CustomUser)
--- server/base/models.py
+++ server/base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
 # from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
@@ -21,11 +21,3 @@ class Experience(models.Model):
 
     def __str__(self):
         return self.company_name
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-
-    # Add custom fields here, if needed
-
-    def __str__(self):
-        return self.username
